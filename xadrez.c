170026836c1385ee01c0217f8920acd484a24357
Wallace676 <wallacestar16@gmail.com>
2025-10-31 02:48:10 +0000

    Implementei o movimento do Cavalo em L (2 para baixo, 1 para esquerda)  usando um while externo e um for interno; o programa imprime cada passo no console.

--- xadrez.c
+++ xadrez.c
@@ -14,17 +14,16 @@ int main()
     // Usei do-wilhe para o movimento do bispo;
     printf("\n=== Movimento do Bispo ===\n");
     int num1 = 0;
-    do{
+    do
+    {
 
         printf("Cima, Direita\n");
         num1++;
 
-
-    }while (num1 < 5);
-
+    } while (num1 < 5);
 
     // ==== Movimento da Rainha ====;
-    // Usei while para o movimento da rainha 
+    // Usei while para o movimento da rainha
     printf("\n=== Movimento da Rainha ===\n");
     int num = 0;
 
@@ -34,5 +33,23 @@ int main()
         num++;
     }
 
-    return 0;// Encerra o programa;
+
+    // ==== Movimento do Cavalo ====;
+    // Usei while e for para o movimento do cavalo;
+     printf("\n === Movimento do cavalo ===\n");
+
+    int passoslaterais = 1;// Quantas vezes o cavalo faz o movimento em L;
+
+
+    while(passoslaterais--)// Decrementa Enquanto ainda tiver movimentos laterais;
+    {
+
+        for (int i = 0; i < 2; i++)// Move duas casas para baixo
+        {
+            printf(" Baixo\n");
+        }
+        printf(" Esquerda\n\n");// Depois move uma casa para a esquerda
+    }
+
+    return 0; // Encerra o programa;
 }
